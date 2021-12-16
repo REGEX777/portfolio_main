831b184b5dff81dd1d7ac3978db4626aebc62408
regex <aditya777regex@gmail.com>
2021-12-16 19:25:03 +0530

    Added everything O.o

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ app.get('/', function (req, res) {
     res.render('index')
 })
 
+app.get('/projects', function(req, res) {
+    res.render('projects')
+})
+app.get('/contact', function(req, res){
+    res.render('contact')
+})
+
 
 
 app.listen(3000, function () {
