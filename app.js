3cab326c4167f59942c7058a810cda8e59b29a66
regex <aditya777regex@gmail.com>
2021-12-26 22:00:39 +0530

    did some changes

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ app.get('/projects', function(req, res) {
 app.get('/contact', function(req, res){
     res.render('contact')
 })
-
-
+app.get('/about', function(req, res){
+    res.render('about')
+})
 
 app.listen(3000, function () {
     console.log('[✅] Server running on port 3000'.green);
